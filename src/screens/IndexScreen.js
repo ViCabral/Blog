d3c4b753b4e07a645ed1c2af0608fda12e9cd1b6
ViCabral <vinicius.cabral@infojrunesp.com.br>
2020-05-07 22:02:27 -0300

    Clear Project

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+
+    return (
+        <>
+        </>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default IndexScreen;
