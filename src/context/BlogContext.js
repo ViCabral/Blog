f8c3601a47b1b7fb7d95f57a5e6308d469ce12d7
ViCabral <vinicius.cabral@infojrunesp.com.br>
2020-05-27 17:31:08 -0300

    useContext fixed and included FlatList in IndexScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,8 +4,13 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
 
+    const blogPosts = [
+        { title: 'Blog Post #0'},
+        { title: 'Blog Post #1'}
+    ];
+
     return (
-    <BlogContext.Provider value={5}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
     );
 };
 
